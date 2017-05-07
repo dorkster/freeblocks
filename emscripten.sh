51ee6540cd3c463e966b91ae3db5703381412ced
Justin Jacobs <jajdorkster@gmail.com>
2017-05-07 14:04:35 -0400

    Add HTML template for Emscripten output

--- emscripten.sh
+++ emscripten.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -f game_html.zip
-emcc src/*.c -O2 -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' -s USE_SDL_TTF=2 --preload-file res -o index.html
-# sed -i '/#output/a display:none !important;' index.html
+emcc src/*.c -O3 -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' -s USE_SDL_TTF=2 --preload-file res -o index.html
+cp index_template.html index.html
 zip -r game_html.zip index.html index.js index.data index.html.mem res/*
