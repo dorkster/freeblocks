bb63754da17bf0de4d10ab28c03ea68822bdca66
Justin Jacobs <jajdorkster@gmail.com>
2017-07-14 16:45:32 -0400

    Use separate directory for emscripten output

--- emscripten.sh
+++ emscripten.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-rm -f game_html.zip
-emcc src/*.c -O3 -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' -s USE_SDL_TTF=2 --preload-file res -o index.html
-cp index_template.html index.html
-zip -r game_html.zip index.html index.js index.data index.html.mem res/*
+mkdir -p emscripten_output
+rm -f emscripten_output/game_html.zip
+emcc src/*.c -O3 -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' -s USE_SDL_TTF=2 --preload-file res -o emscripten_output/index.html
+cp index_template.html emscripten_output/index.html
+cd emscripten_output/
+zip -r game_html.zip index.html index.js index.data index.html.mem ../res/*
